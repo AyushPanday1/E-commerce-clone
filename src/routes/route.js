80434246768dea1a80f22628c818f7c88e7e6294
vikasgautam1234 <vikasmtr1234@gmail.com>
2022-12-20 00:15:38 +0530

    loginwork

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 
 const router = express.Router()
-const productController = require("../controllers/orderController")
+
 const userController=require('../controllers/userController')
 
 router.get('/trial-api' , function(req,res){
    res.send("Yes it is working.")
 })
-router.post('/user-login',productController.userLogin)
+router.post('/register', userController.createUser)
+router.post('/login',userController.userLogin)
 
 module.exports = router;
